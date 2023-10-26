7f7982aa1ecfe7415f0677d053eafec9a43dc0b8
nataliiasolomchak21 <natalia2001727@gmail.com>
2023-10-26 21:34:19 +0100

    Create the admin panel

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-codestar-project-dfb5b4ea5177.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['django-blog-codestar-project-dfb5b4ea5177.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
